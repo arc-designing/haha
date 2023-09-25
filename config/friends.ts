c9f265234bfdc02f6f1c807ced94ec526d0ee596
arc-designing <145824138+arc-designing@users.noreply.github.com>
2023-09-25 14:25:52 +0800

    Update friends.ts

--- config/friends.ts
+++ config/friends.ts
@@ -6,28 +6,28 @@ export type FriendType = {
 
 export const friends: FriendType[] = [
   {
-    url: 'https://blog.yhz610.com/',
-    name: '阿猫阿狗',
+    url: 'https://www.xiaohongshu.com/user/profile/611b7ce10000000001015a52?xhsshare=CopyLink&appuid=62a4b19e000000002102932e&apptime=1695618050',
+    name: '设计标准化',
     img: 'https://cdn.yhz610.com/pic/blog/zhuazi.svg',
   },
   {
-    url: 'https://shiraha.cn/',
-    name: '白羽七海的博客',
+    url: 'https://www.xiaohongshu.com/user/profile/604c276e000000000100b43c?xhsshare=CopyLink&appuid=62a4b19e000000002102932e&apptime=1695618076',
+    name: '设计标准化小助理',
     img: 'https://cdn.jsdelivr.net/gh/45921/cdn-images@main/me/avatar.jpg',
   },
   {
-    url: 'https://blog.cugxuan.cn',
-    name: '泫言',
+    url: 'https://www.xiaohongshu.com/user/profile/5e664670000000000100079e?xhsshare=CopyLink&appuid=62a4b19e000000002102932e&apptime=1695618093',
+    name: '嘎嘎哈哈的空间笔记',
     img: 'https://image.cugxuan.cn/avater.jpg',
   },
   {
-    url: 'https://blog.zhheo.com/',
-    name: '张洪Heo',
+    url: 'https://www.xiaohongshu.com/user/profile/5f42a3660000000001003dc3?xhsshare=CopyLink&appuid=62a4b19e000000002102932e&apptime=1695618113',
+    name: '设计标准化(有料)',
     img: 'https://img.zhheo.com/i/2022/08/19/62ff32fa28da1.png',
   },
   {
-    url: 'https://www.fomal.cc',
-    name: 'Fomalhaut🥝',
+    url: 'https://www.xiaohongshu.com/user/profile/5f42a3660000000001003dc3?xhsshare=CopyLink&appuid=62a4b19e000000002102932e&apptime=1695618113',
+    name: 'Arc-Designing',
     img: 'https://www.fomal.cc/assets/head.jpg',
   },
 ]
